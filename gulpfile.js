e35ec13124f2e3d305f1e2ec51f0ca8c202ccbf4
Joonas Korhonen <joonas.korhonen@sc5.io>
2014-09-02 12:21:54 +0300

    Move frontend code to app folder, public folder contains compiled code now
    Also added gulp tasks for development

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-ruby-sass');
+var watch = require('gulp-watch');
 
 gulp.task('default', function(development) {
 
@@ -13,3 +17,32 @@ gulp.task('default', function(development) {
 
 });
 
+/* Tasks for development */
+
+gulp.task('js', function() {
+  return gulp.src('app/js/**/*.js')
+    .pipe(plumber())
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('sass', function() {
+  return gulp.src('app/sass/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass({
+      compass: true
+    }))
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('watch', function() {
+  process.env['DEBUG'] = 'scyleguide';
+  gulp.start('default');
+
+  watch({glob: 'app/sass/**/*.scss'}, function() {
+      gulp.start('sass');
+  });
+  watch({glob: 'app/js/**/*.js'}, function() {
+      gulp.start('js');
+  });
+});
