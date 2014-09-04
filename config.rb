221d300e43e6f4b0fa56bde7adaaa06968773ca2
Joonas Korhonen <joonas.korhonen@sc5.io>
2014-09-04 12:05:27 +0300

    Change sass_dir in config.rb

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "public/css"
-sass_dir = "public/sass"
+sass_dir = "app/sass"
 images_dir = "public/img"
 javascripts_dir = "public/js"
 
