1c8c0683ae573886933a95fa4f951d6705967df6
Joonas Korhonen <joonas.korhonen@sc5.io>
2014-09-10 11:31:31 +0300

    Move app and public to lib

--- gulpfile.js
+++ gulpfile.js
@@ -8,60 +8,60 @@ var livereload = require('gulp-livereload');
 var styleguide = require('./index.js');
 
 gulp.task('styleguide', function() {
-  return gulp.src(['app/**/*.scss'])
+  return gulp.src(['lib/app/**/*.scss'])
     .pipe(styleguide());
 });
 
 /* Tasks for development */
 
 gulp.task('js:app', function() {
-  return gulp.src(['app/js/**/*.js', '!app/js/vendor/**/*.js'])
+  return gulp.src(['lib/app/js/**/*.js', 'lib/!app/js/vendor/**/*.js'])
     .pipe(plumber())
     .pipe(concat('app.js'))
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('lib/public/js'));
 });
 
 gulp.task('js:vendor', function() {
-  return gulp.src('app/js/vendor/**/*.js')
+  return gulp.src('lib/app/js/vendor/**/*.js')
     .pipe(plumber())
     .pipe(concat('vendor.js'))
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('lib/public/js'));
 });
 
 gulp.task('sass', function() {
-  return gulp.src('app/sass/**/*.scss')
+  return gulp.src('lib/app/sass/**/*.scss')
     .pipe(plumber())
     .pipe(sass({
       
     }))
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('lib/public/css'));
 });
 
 gulp.task('html', function() {
-  return gulp.src('app/**/*.html')
-    .pipe(gulp.dest('dist/'));
+  return gulp.src('lib/app/**/*.html')
+    .pipe(gulp.dest('lib/public/'));
 });
 
 gulp.task('watch', ['sass', 'js:app', 'js:vendor', 'html'], function() {
   livereload.listen();
 
-  watch({glob: 'app/sass/**/*.scss'}, function() {
+  watch({glob: 'lib/app/sass/**/*.scss'}, function() {
       gulp.start('sass');
   });
 
-  watch({glob: ['app/js/**/*.js', '!app/js/vendor/**/*.js']}, function() {
+  watch({glob: ['lib/app/js/**/*.js', '!lib/app/js/vendor/**/*.js']}, function() {
       gulp.start('js:app');
   });
 
-  watch({glob: ['app/js/vendor/**/*.js']}, function() {
+  watch({glob: ['lib/app/js/vendor/**/*.js']}, function() {
       gulp.start('js:vendor');
   });
 
-  watch({glob: 'app/**/*.html'}, function() {
+  watch({glob: 'lib/app/**/*.html'}, function() {
       gulp.start('html');
   });
 
-  gulp.watch('dist/**').on('change', function() {
+  gulp.watch('lib/public/**').on('change', function() {
     gulp.start('styleguide');
     livereload.changed();
   });
