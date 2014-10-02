84394a90185779aee90aa120e875a5c3c68eda4c
Anssi Herranen <anssi.herranen@sc5.io>
2014-10-02 10:50:10 +0300

    Server to run as a task, removed references to lib/dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     livereload = require('gulp-livereload'),
+    neat = require('node-neat'),
     please = require('gulp-pleeease'),
     plumber = require('gulp-plumber'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
-    neat = require('node-neat'),
-    styleguide = require('./lib/styleguide');
+    util = require('gulp-util'),
+
+    styleguide = require('./lib/styleguide'),
+    outputPath = util.env.output ? util.env.output.replace(/\/$/, '') : 'demo/output',
+    sourcePath = util.env.source ? util.env.source.replace(/\/$/, '') : 'demo/source';
 
 /* Tasks for development */
 gulp.task('serve', ['styleguide'], function() {
@@ -37,14 +41,14 @@ gulp.task('js:app', function() {
   return gulp.src(['lib/app/js/**/*.js', '!lib/app/js/vendor/**/*.js'])
     .pipe(plumber())
     .pipe(concat('app.js'))
-    .pipe(gulp.dest('lib/dist/js'));
+    .pipe(gulp.dest(outputPath + '/js'));
 });
 
 gulp.task('js:vendor', function() {
   return gulp.src('lib/app/js/vendor/**/*.js')
     .pipe(plumber())
     .pipe(concat('vendor.js'))
-    .pipe(gulp.dest('lib/dist/js'));
+    .pipe(gulp.dest(outputPath + '/js'));
 });
 
 gulp.task('sass', function() {
@@ -58,27 +62,40 @@ gulp.task('sass', function() {
     .pipe(please({
       minifier: false
     }))
-    .pipe(gulp.dest('lib/dist/css'));
+    .pipe(gulp.dest(outputPath + '/css'));
 });
 
 gulp.task('html', function() {
   return gulp.src('lib/app/**/*.html')
-    .pipe(gulp.dest('lib/dist/'));
+    .pipe(gulp.dest(outputPath + '/'));
 });
 
 gulp.task('assets', function() {
   return gulp.src('lib/app/assets/**')
-    .pipe(gulp.dest('lib/dist/assets/'));
+    .pipe(gulp.dest(outputPath + '/assets'));
 });
 
 gulp.task('watch', ['sass', 'js:app', 'js:vendor', 'html', 'assets'], function() {
-  livereload.listen();
+
+  var app, serverModule, server;
+
+  // TODO: configure livereload
+  // livereload.listen();
+
   gulp.watch('lib/app/sass/**/*.scss', ['sass']);
   gulp.watch(['lib/app/js/**/*.js', '!lib/app/js/vendor/**/*.js'], ['js:app']);
   gulp.watch('lib/app/js/vendor/**/*.js', ['js:vendor']);
   gulp.watch('lib/app/**/*.html', ['html']);
   gulp.watch('demo/source/**', ['styleguide']);
-  gulp.start('serve');
+
+  serverModule = require('./lib/server')(sourcePath, outputPath);
+  app = serverModule.app;
+  server = serverModule.server;
+  app.set('port', util.env.port || 3000);
+  server = server.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + server.address().port);
+  });
+
 });
 
 gulp.task('build', ['sass', 'js:app', 'js:vendor', 'html', 'assets', 'styleguide']);
