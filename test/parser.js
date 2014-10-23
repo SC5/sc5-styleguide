33422b974504253814aadcc89182db52b16fa2a4
Varya Stepanova <mail@varya.me>
2014-10-23 16:57:47 +0300

    Tests on LESS cases

--- test/parser.js
+++ test/parser.js
@@ -6,64 +6,125 @@ var gulp = require('gulp'),
 
 describe('Parser', function() {
   describe('variable parser', function() {
-    it('should parse basic variables', function() {
-      var str = multiline(function() {
-        /*
-$mycolor: #00ff00;
-$mypadding: 3px;
-$myfont:   "Helvetica Neue", Helvetica, Arial, sans-serif;
-        */
-      }),
-      result = {
-        mycolor: '#00ff00',
-        mypadding: '3px',
-        myfont: '"Helvetica Neue", Helvetica, Arial, sans-serif'
-      };
-      expect(parser.parseVariables(str)).eql(result);
-    });
+    describe('SCSS syntax', function() {
+      it('should parse basic variables', function() {
+        var str = multiline(function() {
+          /*
+  $mycolor: #00ff00;
+  $mypadding: 3px;
+  $myfont:   "Helvetica Neue", Helvetica, Arial, sans-serif;
+          */
+        }),
+        result = {
+          mycolor: '#00ff00',
+          mypadding: '3px',
+          myfont: '"Helvetica Neue", Helvetica, Arial, sans-serif'
+        };
+        expect(parser.parseVariables(str)).eql(result);
+      });
 
-    it('should parse variables from file with containing comments and intended lines', function() {
-      var str = multiline(function() {
-        /*
-$mycolor: #00ff00;
-// Test comment
-    $mypadding: 3px; // Test comment 2
-$myfont: "Helvetica Neue", Helvetica, Arial, sans-serif;
-        */
-      }),
-      result = {
-        mycolor: '#00ff00',
-        mypadding: '3px',
-        myfont: '"Helvetica Neue", Helvetica, Arial, sans-serif'
-      };
-      expect(parser.parseVariables(str)).eql(result);
-    });
+      it('should parse variables from file with containing comments and intended lines', function() {
+        var str = multiline(function() {
+          /*
+  $mycolor: #00ff00;
+  // Test comment
+      $mypadding: 3px; // Test comment 2
+  $myfont: "Helvetica Neue", Helvetica, Arial, sans-serif;
+          */
+        }),
+        result = {
+          mycolor: '#00ff00',
+          mypadding: '3px',
+          myfont: '"Helvetica Neue", Helvetica, Arial, sans-serif'
+        };
+        expect(parser.parseVariables(str)).eql(result);
+      });
 
-    it('should parse variables correct when there are multiple variables in a single line', function() {
-      var str = multiline(function() {
-        /*
-$color1: #ff0000; $color2: #00ff00; $color3: #0000ff;
-        */
-      }),
-      result = {
-        color1: '#ff0000',
-        color2: '#00ff00',
-        color3: '#0000ff'
-      };
+      it('should parse variables correct when there are multiple variables in a single line', function() {
+        var str = multiline(function() {
+          /*
+  $color1: #ff0000; $color2: #00ff00; $color3: #0000ff;
+          */
+        }),
+        result = {
+          color1: '#ff0000',
+          color2: '#00ff00',
+          color3: '#0000ff'
+        };
+      });
+      it('should not take commented variables', function() {
+        var str = multiline(function() {
+          /*
+  $color1: #ff0000;
+  // $color2: #00ff00;
+  $color3: #0000ff;
+  // $color4: #0f0f0f;
+          */
+        }),
+        result = {
+          color1: '#ff0000',
+          color3: '#0000ff'
+        };
+      });
     });
-    it('should not take commented variables', function() {
-      var str = multiline(function() {
-        /*
-$color1: #ff0000;
-// $color2: #00ff00;
-$color3: #0000ff;
-// $color4: #0f0f0f;
-        */
-      }),
-      result = {
-        color1: '#ff0000',
-        color3: '#0000ff'
-      };
+    describe('LESS syntax', function() {
+      it('should parse basic variables', function() {
+        var str = multiline(function() {
+          /*
+  @mycolor: #00ff00;
+  @mypadding: 3px;
+  @myfont:   "Helvetica Neue", Helvetica, Arial, sans-serif;
+          */
+        }),
+        result = {
+          mycolor: '#00ff00',
+          mypadding: '3px',
+          myfont: '"Helvetica Neue", Helvetica, Arial, sans-serif'
+        };
+        expect(parser.parseVariables(str, 'less')).eql(result);
+      });
+      it('should parse variables from file with containing comments and intended lines', function() {
+        var str = multiline(function() {
+          /*
+  @mycolor: #00ff00;
+  // Test comment
+    @mypadding: 3px; // Test comment 2
+  @myfont: "Helvetica Neue", Helvetica, Arial, sans-serif;
+          */
+        }),
+        result = {
+          mycolor: '#00ff00',
+          mypadding: '3px',
+          myfont: '"Helvetica Neue", Helvetica, Arial, sans-serif'
+        };
+        expect(parser.parseVariables(str, 'less')).eql(result);
+      });
+      it('should parse variables correct when there are multiple variables in a single line', function() {
+        var str = multiline(function() {
+          /*
+  @color1: #ff0000; @color2: #00ff00; @color3: #0000ff;
+          */
+        }),
+        result = {
+          color1: '#ff0000',
+          color2: '#00ff00',
+          color3: '#0000ff'
+        };
+      });
+      it('should not take commented variables', function() {
+        var str = multiline(function() {
+          /*
+  @color1: #ff0000;
+  // @color2: #00ff00;
+  @color3: #0000ff;
+  // @color4: #0f0f0f;
+          */
+        }),
+        result = {
+          color1: '#ff0000',
+          color3: '#0000ff'
+        };
+      });
     });
   });
 
