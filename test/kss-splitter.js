824733ad0d068c992939480a726b1238aeaa68b8
Varya Stepanova <mail@varya.me>
2014-11-07 14:24:10 +0200

    Code can be complex

--- test/kss-splitter.js
+++ test/kss-splitter.js
@@ -59,4 +59,33 @@ describe('KSS divider', function() {
     expect(kssBlocks).eql(result);
   });
 
+  it('should take multiline code', function(){
+    var str = multiline(function() {
+      /*
+// Comment
+//Styleguide 1.0 
+
+.a { b: c }
+$a: b;
+
+.x { y: z }
+      */
+    }),
+    result = [
+      [
+        'block',
+        [
+          'kss',
+          '// Comment\n//Styleguide 1.0 \n'
+        ],
+        [
+          'code',
+          '.a { b: c }\n$a: b;\n\n.x { y: z }'
+        ]
+      ]
+    ],
+    kssBlocks = kssSplitter.getAst(str);
+    expect(kssBlocks).eql(result);
+  });
+
 });
