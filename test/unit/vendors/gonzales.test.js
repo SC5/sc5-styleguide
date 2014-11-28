d367f4438e909bdbf9289a06656b94acb019306f
Varya Stepanova <mail@varya.me>
2014-11-28 11:19:34 +0200

    Make tests on Gonzales more atomic

--- test/unit/vendors/gonzales.test.js
+++ test/unit/vendors/gonzales.test.js
@@ -5,78 +5,69 @@ var requireModule = require('requirefrom')('lib/modules'),
     multiline = require('multiline'),
     gonzales = require('gonzales-pe');
 
-var util = require('util');
-
 describe('Gonzales', function() {
 
-  describe('Should parse Bootsrap variables', function() {
-    var str = multiline(function() {
-      /*
-$bootstrap-sass-asset-helper: (twbs-font-path("") != unquote('twbs-font-path("")')) !default;
-$font-size-large: ceil(($font-size-base * 1.25)) !default; // ~18px
-      */
-    }),
-      result = [ 'stylesheet',
-        [ 'declaration',
-          [ 'property',
-            [ 'variable', [ 'ident', 'bootstrap-sass-asset-helper' ] ] ],
-          [ 'propertyDelim' ],
-          [ 's', ' ' ],
-          [ 'value',
-            [ 'braces',
-              '(',
-              ')',
-              [ 'function',
-                [ 'ident', 'twbs-font-path' ],
-                [ 'arguments', [ 'string', '""' ] ] ],
-              [ 's', ' ' ],
-              [ 'operator', '!=' ],
-              [ 's', ' ' ],
-              [ 'function',
-                [ 'ident', 'unquote' ],
-                [ 'arguments', [ 'string', '\'twbs-font-path("")\'' ] ] ] ],
-            [ 's', ' ' ],
-            [ 'default' ] ] ],
-        [ 'declDelim' ],
-        [ 's', '\n' ],
-        [ 'declaration',
-          [ 'property', [ 'variable', [ 'ident', 'font-size-large' ] ] ],
-          [ 'propertyDelim' ],
-          [ 's', ' ' ],
-          [ 'value',
-            [ 'function',
-              [ 'ident', 'ceil' ],
-              [ 'arguments',
-                [ 'braces',
-                  '(',
-                  ')',
-                  [ 'variable', [ 'ident', 'font-size-base' ] ],
-                  [ 's', ' ' ],
-                  [ 'ident', '*' ],
-                  [ 's', ' ' ],
-                  [ 'number', '1.25' ] ] ] ],
-            [ 's', ' ' ],
-            [ 'default' ] ] ],
-        [ 'declDelim' ],
-        [ 's', ' ' ],
-        [ 'commentSL', ' ~18px' ] ];
+  describe('SASS', function() {
 
-    it('SASS', function() {
+    it('Should take inequality', function() {
+      var str = multiline(function() {
+        /*
+$a: 1 != 2;
+        */
+      });
+
       ast = gonzales.srcToAST({
         src: str,
         syntax: 'sass'
       });
-      expect(ast).eql(result);
+      expect(ast).to.be.an('array');
     });
 
-    it('SCSS', function() {
+    it('Should take multiple braces in functions', function() {
+      var str = multiline(function() {
+        /*
+$a: cell((1.75));
+        */
+      });
+
+      ast = gonzales.srcToAST({
+        src: str,
+        syntax: 'sass'
+      });
+      expect(ast).to.be.an('array');
+    });
+
+  });
+
+  describe('SCSS', function() {
+
+    it('Should take inequality', function() {
+      var str = multiline(function() {
+        /*
+$a: 1 != 2;
+        */
+      });
+
       ast = gonzales.srcToAST({
         src: str,
         syntax: 'scss'
       });
-      expect(ast).eql(result);
+      expect(ast).to.be.an('array');
     });
 
+    it('Should take multiple braces in functions', function() {
+      var str = multiline(function() {
+        /*
+$a: cell((1.75));
+        */
+      });
+
+      ast = gonzales.srcToAST({
+        src: str,
+        syntax: 'scss'
+      });
+      expect(ast).to.be.an('array');
+    });
   });
 
 });
