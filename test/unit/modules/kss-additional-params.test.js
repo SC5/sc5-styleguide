59e74ff7998e0ba3ba774c9f453d56b38a723ae2
Varya Stepanova <mail@varya.me>
2014-12-09 15:44:35 +0200

    A module parsing additional KSS params

---
+++ test/unit/modules/kss-additional-params.test.js
@@ -0,0 +1,110 @@
+//jscs:disable disallowTrailingWhitespace
+//jscs:disable disallowMultipleLineBreaks
+var requireModule = require('requirefrom')('lib/modules'),
+    chai = require('chai'),
+    expect = chai.expect,
+    multiline = require('multiline'),
+    kssAdditionalParams = requireModule('kss-additional-params');
+
+describe('Parsing KSS additional params', function() {
+
+  it('Should parse from singleline-commented block', function() {
+    var str = multiline(function() {
+      /*
+// sg-param:
+// Value
+      */
+      }),
+      result = { 'sg-param': ' Value' },
+      params = kssAdditionalParams.get(str);
+
+    expect(params).eql(result);
+  });
+
+  it('Should parse from multiline-commented block', function() {
+
+    var str = '' +
+        '/*\n' +
+        ' sg-param:\n' +
+        ' Value' +
+        '*/',
+      result = { 'sg-param': ' Value' },
+      params = kssAdditionalParams.get(str);
+
+    expect(params).eql(result);
+  });
+
+  it('Should parse multiple params', function() {
+    var str = multiline(function() {
+      /*
+// sg-param1:
+// Value
+//
+// sg-param2:
+// Value
+//
+// sg-param3:
+// Value
+      */
+      }),
+      result = {
+        'sg-param1':' Value',
+        'sg-param2':' Value',
+        'sg-param3':' Value'
+      },
+      params = kssAdditionalParams.get(str);
+
+    expect(params).eql(result);
+  });
+
+  it('Should gulp different space combinations', function() {
+    var str = multiline(function() {
+      /*
+// sg-param1 :
+// Value
+//
+//sg-param2:
+// Value
+//
+//   sg-param3:
+// Value
+      */
+      }),
+      result = {
+        'sg-param1':' Value',
+        'sg-param2':' Value',
+        'sg-param3':' Value'
+      },
+      params = kssAdditionalParams.get(str);
+
+    expect(params).eql(result);
+  });
+
+  it('Should ignore extra comments', function() {
+    var str = multiline(function() {
+      /*
+// Something here
+//
+// sg-param1 :
+// Value
+//
+//sg-param2:
+// Value
+//
+// sg-empty-param:
+//
+//   sg-param3:
+// Value
+      */
+      }),
+      result = {
+        'sg-param1':' Value',
+        'sg-param2':' Value',
+        'sg-param3':' Value'
+      },
+      params = kssAdditionalParams.get(str);
+
+    expect(params).eql(result);
+  });
+
+});
