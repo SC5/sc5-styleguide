44d92bd6689dda280b6fcb8ea8907e4e54d28145
Varya Stepanova <mail@varya.me>
2015-01-08 14:50:30 +0200

    More tests

--- test/unit/modules/kss-sanitizer.test.js
+++ test/unit/modules/kss-sanitizer.test.js
@@ -9,7 +9,8 @@ var requireModule = require('requirefrom')('lib/modules'),
 describe('KSS sanitizer', function() {
 
   describe('Single line comments', function() {
-    it('Split 2 blocks of singleline comments', function() {
+
+    it('should sanitize sg-prefixed parameter', function() {
       var str = multiline(function() {
         /*
 // Test
@@ -27,6 +28,110 @@ describe('KSS sanitizer', function() {
 // Test
         */
         }),
+        sanitized = kssSanitizer(str);
+
+      expect(sanitized).eql(result);
+
+    });
+
+    it('should sanitize not sanitize other parameters', function() {
+      var str = multiline(function() {
+        /*
+// Test
+//
+// markup:
+//
+// Test
+        */
+        }),
+        result = multiline(function() {
+        /*
+// Test
+//
+// markup:
+//
+// Test
+        */
+        }),
+        sanitized = kssSanitizer(str);
+
+      expect(sanitized).eql(result);
+
+    });
+
+    it('should sanitize not sanitize the whole paragraph', function() {
+      var str = multiline(function() {
+        /*
+// Test
+//
+// sg-wrapper:
+// <div>
+//  <span></span>
+// </div>
+//
+// Test
+        */
+        }),
+        result = multiline(function() {
+        /*
+// Test
+//
+//
+// Test
+        */
+        }),
+        sanitized = kssSanitizer(str);
+
+      expect(sanitized).eql(result);
+
+    });
+
+    it('should allow spaces in empty strings', function() {
+      var str = multiline(function() {
+        /*
+// Test
+// 
+// sg-param:
+// Test
+// 
+// Test
+        */
+        }),
+        result = multiline(function() {
+        /*
+// Test
+// 
+// 
+// Test
+        */
+        }),
+        sanitized = kssSanitizer(str);
+
+      expect(sanitized).eql(result);
+
+    });
+
+  });
+  describe('Multi line comments', function() {
+
+    it('should sanitize sg-prefixed parameter', function() {
+      var str = '/*\n' + multiline(function() {
+        /*
+Test
+
+sg-angular:
+
+Test
+        */
+        }) + '\n*/',
+        result = '/*\n' + multiline(function() {
+        /*
+Test
+
+
+Test
+        */
+        }) + '\n*/',
         sanitized = kssSanitizer(str);
 
       expect(sanitized).eql(result);
