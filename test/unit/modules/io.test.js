44ae9d8564e2ede08dbbb4811408e0e848ecca7b
Juuso Backman <juuso.backman@sc5.io>
2015-01-16 15:57:00 +0200

    Rewrite variable saving logic; don't save anything if any variable syntax check fails.

--- test/unit/modules/io.test.js
+++ test/unit/modules/io.test.js
@@ -12,182 +12,295 @@ chai.use(sinonChai);
 
 describe('module io', function() {
 
-  var fs, writer, server, socket, opt, ioModule, io;
+  var fs, parser, writer, server, sockets, opt, ioModule, io;
   beforeEach(setUp);
 
   describe('emitProgressStart()', function() {
 
-    it('emits socket event "styleguide progress start"', function() {
+    it('emits "styleguide progress start" to all sockets', function() {
       io.emitProgressStart();
-      expect(socket.emit).to.have.been.calledWith('styleguide progress start');
+      expect(sockets.emit).to.have.been.calledWithExactly('styleguide progress start');
+    });
+
+  });
+
+  describe('emitProgressEnd()', function() {
+
+    it('emits "styleguide progress end" to all sockets', function() {
+      io.emitProgressEnd();
+      expect(sockets.emit).to.have.been.calledWithExactly('styleguide progress end');
     });
 
   });
 
   describe('emitCompileSuccess()', function() {
 
-    it('emits socket event "styleguide compile success"', function() {
+    it('emits "styleguide compile success" to all sockets if called without arguments', function() {
       io.emitCompileSuccess();
-      expect(socket.emit).to.have.been.calledWith('styleguide compile success');
+      expect(sockets.emit).to.have.been.calledWithExactly('styleguide compile success');
     });
 
-    it('emits socket event "styleguide progress end"', function() {
-      io.emitCompileSuccess();
-      expect(socket.emit).to.have.been.calledWith('styleguide progress end');
+    it('emits "styleguide compile success" to specified socket', function() {
+      var socket = fakeSocket();
+      io.emitCompileSuccess(socket);
+      expect(socket.emit).to.have.been.calledWithExactly('styleguide compile success');
+      expect(sockets.emit).not.to.have.been.called;
     });
 
   });
 
   describe('emitCompileError()', function() {
 
-    it('emits socket event "styleguide compile error" with error data', function() {
+    it('emits "styleguide compile error" with error to all sockets if called only with error argument', function() {
       var error = { message: 'fail' };
       io.emitCompileError(error);
-      expect(socket.emit).to.have.been.calledWith('styleguide compile error', error);
+      expect(sockets.emit).to.have.been.calledWithExactly('styleguide compile error', error);
     });
 
-    it('emits socket event "styleguide progress end"', function() {
-      io.emitCompileError();
-      expect(socket.emit).to.have.been.calledWith('styleguide progress end');
+    it('emits "styleguide compile error" with error to specified socket', function() {
+      var error = { message: 'fail' },
+          socket = fakeSocket();
+      io.emitCompileError(error, socket);
+      expect(socket.emit).to.have.been.calledWithExactly('styleguide compile error', error);
+      expect(sockets.emit).not.to.have.been.called;
+    });
+
+  });
+
+  describe('emitStylesChanged', function() {
+
+    it('emits "styleguide styles changed" to all sockets', function() {
+      io.emitStylesChanged();
+      expect(sockets.emit).to.have.been.calledWithExactly('styleguide styles changed');
     });
 
   });
 
   describe('socket connection listener', function() {
-    var listener;
+    var listener, socket;
 
     beforeEach(function() {
       listener = server.on.getCall(0).args[1];
+      socket = fakeSocket();
     });
 
     it('is registered on socket "connection" event', function() {
-      expect(server.on).to.have.been.calledWith('connection');
+      expect(server.on).to.have.been.calledWithExactly('connection', sinon.match.func);
       expect(listener).to.be.a('function');
     });
 
     it('registers listener on "variables to server" event', function() {
       listener.call(undefined, socket);
-      expect(socket.on).to.have.been.calledWith('variables to server');
+      expect(socket.on).to.have.been.calledWithExactly('variables to server', sinon.match.func);
     });
 
     it('emits "styleguide compile success" on connection event if previous compile succeeded', function() {
       io.emitCompileSuccess();
-      socket.emit.reset();
       listener.call(undefined, socket);
-      expect(socket.emit).to.have.been.calledWith('styleguide compile success').and.calledWith('styleguide progress end');
+      expect(socket.emit).to.have.been.calledWithExactly('styleguide compile success');
     });
 
     it('emits "styleguide compile error" on connection event if previous compile failed', function() {
-      io.emitCompileError();
-      socket.emit.reset();
+      var error = Error('foo is bar');
+      io.emitCompileError(error);
       listener.call(undefined, socket);
-      expect(socket.emit).to.have.been.calledWith('styleguide compile error').and.calledWith('styleguide progress end');
+      expect(socket.emit).to.have.been.calledWithExactly('styleguide compile error', error);
     });
   });
 
-  describe('save variables', function() {
+  describe('#saveVariables', function() {
     var newVariables = [
-      {
-        file: 'first_file.less',
-        fileHash: 'ab',
-        name: 'myvar1',
-        value: 'myvalue1'
-      },
-      {
-        file: 'second_file.scss',
-        fileHash: 'cd',
-        name: 'myvar3',
-        value: 'myvalue3'
-      },
-      {
-        file: 'first_file.less',
-        fileHash: 'ab',
-        name: 'myvar2',
-        value: 'myvalue2'
-      }
-    ];
+        {
+          file: 'first_file.less',
+          fileHash: 'first',
+          name: 'myvar1',
+          value: 'myvalue1'
+        },
+        {
+          file: 'second_file.scss',
+          fileHash: 'second',
+          name: 'myvar3',
+          value: 'myvalue3'
+        },
+        {
+          file: 'first_file.less',
+          fileHash: 'first',
+          name: 'myvar2',
+          value: 'myvalue2'
+        }
+      ],
+      fsOpts = { encoding: 'utf8' };
 
-    beforeEach(function(done) {
+    beforeEach(function() {
       opt.styleVariables = 'test/vars.scss';
-      opt.fileHashes.ab = '/absolute/path/first_file.less';
-      opt.fileHashes.cd = '/absolute/path/second_file.scss';
+      opt.fileHashes.first = '/absolute/path/first_file.less';
+      opt.fileHashes.second = '/absolute/path/second_file.scss';
 
-      // Stub the file system module
-      sinon.stub(fs, 'readFile');
-      sinon.stub(fs, 'writeFile');
+      fs.readFileSync
+        .withArgs(opt.fileHashes.first, fsOpts)
+        .returns('First file content');
 
-      fs.readFile
-        .withArgs('/absolute/path/first_file.less', sinon.match.any, sinon.match.func)
-        .callsArgWith(2, null, 'First file content');
+      fs.readFileSync
+        .withArgs(opt.fileHashes.second, fsOpts)
+        .returns('Second file content');
+    });
 
-      fs.readFile
-        .withArgs('/absolute/path/second_file.scss', sinon.match.any, sinon.match.func)
-        .callsArgWith(2, null, 'Second file content');
+    describe('in the happy case scenario', function() {
 
-      fs.writeFile
-        .withArgs('/absolute/path/first_file.less', sinon.match.any, sinon.match.func)
-        .callsArgWith(2, null, 'Changed first file content');
+      it('reads each variable file once', function(done) {
+        io.saveVariables(newVariables).done(function() {
+          expect(fs.readFileSync).to.have.been.calledWithExactly(opt.fileHashes.first, fsOpts);
+          expect(fs.readFileSync).to.have.been.calledWithExactly(opt.fileHashes.second, fsOpts);
+          expect(fs.readFileSync).to.have.been.calledTwice;
+          done();
+        }, done);
+      });
 
-      fs.writeFile
-        .withArgs('/absolute/path/second_file.scss', sinon.match.any, sinon.match.func)
-        .callsArgWith(2, null, 'Changed cecond file content');
+      it('replaces variables in each file with new variables', function(done) {
+        var firstFileVars = [newVariables[0], newVariables[2]],
+          secondFileVars = [newVariables[1]];
+        io.saveVariables(newVariables).done(function() {
+          expect(writer.setVariables).to.have.been.calledWithExactly('First file content', 'less', firstFileVars);
+          expect(writer.setVariables).to.have.been.calledWithExactly('Second file content', 'scss', secondFileVars);
+          expect(writer.setVariables).to.have.been.calledTwice;
+          done();
+        }, done);
+      });
 
-      io.saveVariables(newVariables).then(function() {
+      it('checks new variable syntax for each file with variable-parser', function(done) {
+        writer.setVariables.withArgs('First file content', 'less').returns('updated first file');
+        writer.setVariables.withArgs('Second file content', 'scss').returns('updated second file');
+        io.saveVariables(newVariables).done(function() {
+          expect(parser.parseVariableDeclarations).to.have.been.calledWithExactly('updated first file', 'less');
+          expect(parser.parseVariableDeclarations).to.have.been.calledWithExactly('updated second file', 'scss');
+          expect(parser.parseVariableDeclarations).to.have.been.calledTwice;
+          done();
+        }, done);
+      });
+
+      it('writes new contents for each file', function(done) {
+        writer.setVariables.withArgs('First file content', 'less').returns('updated first file');
+        writer.setVariables.withArgs('Second file content', 'scss').returns('updated second file');
+        io.saveVariables(newVariables).done(function() {
+          expect(fs.writeFileSync).to.have.been.calledWithExactly(opt.fileHashes.first, 'updated first file', fsOpts);
+          expect(fs.writeFileSync).to.have.been.calledWithExactly(opt.fileHashes.second, 'updated second file', fsOpts);
+          expect(fs.writeFileSync).to.have.been.calledTwice;
+          done();
+        }, done);
+      });
+
+      it('emits "variables saved to server" via same socket on success', function(done) {
+        var sock = {
+          variableSaveListener: null,
+          conn: { id: 1 },
+          on: function(event, fn) {
+            this.variableSaveListener = fn;
+          },
+          emit: function(event) {
+            if (event === 'variables saved to server') {
+              done();
+            }
+          }
+        };
+        server.on.lastCall.args[1].call(undefined, sock);
+        sock.variableSaveListener(newVariables);
+      });
+
+    });
+
+    it('does not update any files if reading the original file fails', function(done) {
+      fs.readFileSync = sinon.stub().throws(Error('ENOENT'));
+      io.saveVariables(newVariables).done(function() {
+        done(Error('expected promise to be rejected due to fs.readFileSync error'));
+      }, function(err) {
+        expect(err.message).to.eql('ENOENT');
+        expect(fs.writeFileSync).not.to.have.been.called;
         done();
       });
     });
 
-    it('should call set variables with the original file contents and variables from that file', function() {
-      var firstFileVars = [{
-        file: 'first_file.less',
-        fileHash: 'ab',
-        name: 'myvar1',
-        value: 'myvalue1'
-      },
-      {
-        file: 'first_file.less',
-        fileHash: 'ab',
-        name: 'myvar2',
-        value: 'myvalue2'
-      }],
-      secondFileVars = [{
-        file: 'second_file.scss',
-        fileHash: 'cd',
-        name: 'myvar3',
-        value: 'myvalue3'
-      }];
-      expect(writer.setVariables).to.have.been.calledWith('First file content', 'less', firstFileVars);
-      expect(writer.setVariables).to.have.been.calledWith('Second file content', 'scss', secondFileVars);
+    it('does not update any files if updating the variable values fails', function(done) {
+      writer.setVariables.throws(Error('cannot update'));
+      io.saveVariables(newVariables).done(function() {
+        done(Error('expected promise to be rejected due to variable-writer error'));
+      }, function(err) {
+        expect(err.message).to.eql('cannot update');
+        expect(fs.writeFileSync).not.to.have.been.called;
+        done();
+      });
+    });
+
+    it('does not update any files if checking a file syntax fails', function(done) {
+      parser.parseVariableDeclarations.throws(Error('invalid syntax'));
+      io.saveVariables(newVariables).done(function() {
+        done(Error('expected promise to be rejected due to variable-parser error'));
+      }, function(err) {
+        expect(err.message).to.eql('invalid syntax');
+        expect(fs.writeFileSync).not.to.have.been.called;
+        done();
+      });
+    });
+
+    it('emits "styleguide compile error" with error via same socket if saving variables fails', function(done) {
+      var error = Error('invalid syntax'),
+        sock = {
+        variableSaveListener: null,
+        conn: { id: 1 },
+        on: function(event, fn) {
+          this.variableSaveListener = fn;
+        },
+        emit: function(event, data) {
+          if (event === 'styleguide compile error') {
+            expect(data).to.eql(error);
+            done();
+          }
+        }
+      };
+      parser.parseVariableDeclarations.throws(error);
+      server.on.lastCall.args[1].call(undefined, sock);
+      sock.variableSaveListener(newVariables);
     });
 
   });
 
   function setUp() {
-    socket = {
+    sockets = {
       conn: { id: '123' },
       emit: sinon.spy(),
       on: sinon.spy()
     };
 
     server = {
-      sockets: socket,
+      sockets: sockets,
       on: sinon.spy()
     };
 
     opt = {
-      styleVariables: null,
       fileHashes: {}
     };
 
-    fs = {};
-    writer = { setVariables: sinon.spy() };
+    fs = {
+      readFileSync: sinon.stub(),
+      writeFileSync: sinon.stub()
+    };
+    parser = { parseVariableDeclarations: sinon.stub() };
+    writer = { setVariables: sinon.stub() };
 
     ioModule = proxyquire(ioPath, {
       fs: fs,
+      './variable-parser': parser,
       './variable-writer': writer
     });
 
     io = ioModule(server, opt);
+  }
+
+  function fakeSocket() {
+    return {
+      conn: { id: 1 },
+      on: sinon.spy(),
+      emit: sinon.spy()
+    };
   }
 
 });
