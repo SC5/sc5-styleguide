bba91067331983868c7019bd657564c882e6683c
Juuso Backman <juuso.backman@sc5.io>
2015-01-29 22:04:12 +0200

    Resolve test config paths with node.path

--- test/integration/test-config.js
+++ test/integration/test-config.js
@@ -1,12 +1,14 @@
+var path = require('path');
+
 module.exports = {
   title: 'Test Styleguide',
-  overviewPath: 'test/projects/scss-project/source/test_overview.md',
+  overviewPath: path.resolve(__dirname, '../projects/scss-project/source/test_overview.md'),
   appRoot: '/my-styleguide-book',
   extraHead: [
     '<link rel="stylesheet" type="text/css" href="your/custom/style.css">',
     '<script src="your/custom/script.js"></script>'
   ],
   commonClass: ['custom-class-1', 'custom-class-2'],
-  styleVariables: 'test/projects/scss-project/source/styles/_styleguide_variables.scss',
+  styleVariables: path.resolve(__dirname, '../projects/scss-project/source/styles/_styleguide_variables.scss'),
   filesConfig: []
 };
