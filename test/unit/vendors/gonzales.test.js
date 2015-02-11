7e2e00a83957a68e0e6851427355a5f47b8188db
Juuso Backman <juuso.backman@sc5.io>
2015-02-11 11:41:02 +0200

    Revert gonzales parsing related files back to older versions

--- test/unit/vendors/gonzales.test.js
+++ test/unit/vendors/gonzales.test.js
@@ -1,64 +1,73 @@
 var chai = require('chai'),
     expect = chai.expect,
+    multiline = require('multiline'),
     gonzales = require('gonzales-pe');
 
-describe('gonzales-pe', function() {
+describe('Gonzales', function() {
 
-  var syntax = {
-    sass: {
-      name: 'sass',
-      inequality: '$a: 1 != 2',
-      braces: '$a: ceil((1.75))',
-      error: '$a = foo'
-    },
-    scss: {
-      name: 'scss',
-      inequality: '$a: 1 != 2',
-      braces: '$a: ceil((1.75))',
-      error: '$a = foo'
-    }
-  };
+  var ast;
 
-  checkSyntax(syntax.sass);
-  checkSyntax(syntax.scss);
+  describe('SASS', function() {
 
-});
+    it('Should take inequality', function() {
+      var str = multiline(function() {
+        /*
+$a: 1 != 2;
+        */
+      });
 
-function checkSyntax(syntax) {
-
-  describe(syntax.name, function() {
-
-    var ast;
-
-    it('should parse inequality', function() {
-      ast = parse(syntax.name, syntax.inequality);
-      expect(ast).to.be.an('object');
-      expect(ast.type).to.eql('stylesheet');
+      ast = gonzales.srcToAST({
+        src: str,
+        syntax: 'sass'
+      });
+      expect(ast).to.be.an('array');
     });
 
-    it('should parse multiple braces in functions', function() {
-      ast = parse(syntax.name, syntax.braces);
-      expect(ast).to.be.an('object');
-      expect(ast.type).to.eql('stylesheet');
+    it('Should take multiple braces in functions', function() {
+      var str = multiline(function() {
+        /*
+$a: cell((1.75));
+        */
+      });
+
+      ast = gonzales.srcToAST({
+        src: str,
+        syntax: 'sass'
+      });
+      expect(ast).to.be.an('array');
     });
 
-    it('should return error message with invalid syntax', function(done) {
-      try {
-        parse(syntax.name, syntax.error);
-        done(new Error('No error was thrown'));
-      } catch (e) {
-        var msg = e.toString();
-        expect(e.line).to.eql(1);
-        expect(e.syntax).to.eql(syntax.name);
-        expect(msg).to.contain('Parsing error: Please check the validity of the block starting from line #1');
-        expect(msg).to.contain('1*| ' + syntax.error);
-        done();
-      }
+  });
+
+  describe('SCSS', function() {
+
+    it('Should take inequality', function() {
+      var str = multiline(function() {
+        /*
+$a: 1 != 2;
+        */
+      });
+
+      ast = gonzales.srcToAST({
+        src: str,
+        syntax: 'scss'
+      });
+      expect(ast).to.be.an('array');
+    });
+
+    it('Should take multiple braces in functions', function() {
+      var str = multiline(function() {
+        /*
+$a: cell((1.75));
+        */
+      });
+
+      ast = gonzales.srcToAST({
+        src: str,
+        syntax: 'scss'
+      });
+      expect(ast).to.be.an('array');
     });
   });
 
-}
-
-function parse(syntax, str) {
-  return gonzales.parse(str, { syntax: syntax });
-}
+});
